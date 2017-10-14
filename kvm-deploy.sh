352446ae90ce7c943bac2baea069b827db0da5cb
Jackson Hughes <jxhughes95@gmail.com>
2017-10-14 23:51:02 +0100

    added stuff

--- kvm-deploy.sh
+++ kvm-deploy.sh
@@ -3,13 +3,14 @@
 # Function: Deploy KVM VMs from template
 
 HOSTNAME=$1
+VMNAME=$1 | awk '{print toupper($0)}'
 
 # Clone new VM from template
 
 virt-clone \
 #--connect $KVM_HYPERVISOR
 --original centos7-template \
---name ${$1^^} \
+--name $VMNAME \
 --file /kvm-vm-storage/$1.qcow2
 
 virt-sysprep \
@@ -20,4 +21,4 @@ virt-sysprep \
 --hostname $1 \
 --update
 
-virsh start ${$1^^}
+virsh start $VMNAME
