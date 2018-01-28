4637ead6b8d44ef3eef59683cde15912ad4f51a1
Jackson Hughes <jxhughes95@gmail.com>
2018-01-28 21:26:17 +0000

    got to initial working state

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,19 @@
 # Function: Deploy KVM node from template
 
 HOSTNAME=$1
-FIRSTBOOT-SCRIPT=/vault/kvm-vm-templates/script.sh
+FIRSTBOOTSCRIPT=/vault/kvm-vm-templates/script.sh
 
 # Create new node from template
 virt-clone \
 --original-xml /vault/kvm-vm-templates/ubuntu16.04-template.xml \
---name $HOSTNAME 
+--name $HOSTNAME \
 --file /vault/kvm-vm-storage/$HOSTNAME.qcow2
 
 # Sysprep newly created node - including update and firstboot script. 
 virt-sysprep \
 --domain $HOSTNAME \
---colours \
 --hostname $HOSTNAME \
---firstboot $FIRSTBOOT-SCRIPT
- 
+--firstboot $FIRSTBOOTSCRIPT
+
 # Start node
-virsh start $HOSTNAME  
+virsh start $HOSTNAME
