47e201a1e5de98b3613aec90b41299c52448e47f
Jackson Mark Hughes <jxhughes95@gmail.com>
2018-07-18 23:32:26 +0100

    Added error handling for arguments and if statement for OS choice.

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,32 @@
 # Author: Jackson
 # Function: Deploy KVM node from template
 
+set -xe
+
 HOSTNAME=$1
 FIRSTBOOTSCRIPT=/vault/kvm-vm-templates/script.sh
 
+display_usage() {
+    echo -e '\nUsage: This script requires two arguments, hostname and OS of the VM to be deployed\n\nE.g. bash deploy.sh test-machine centos\n'
+}
+
+if [ $# -eq 0 ]; then
+    display_usage
+    exit 1
+fi
+
+if [ "$2" == 'centos' ]; then
+    TEMPLATE='centos7-template'
+elif [ "$2" == 'ubuntu' ]; then
+    TEMPLATE='ubuntu16.04-template'
+else
+    display_usage
+    exit1
+fi
+
 # Create new node from template
 virt-clone \
---original-xml /vault/kvm-vm-templates/ubuntu16.04-template.xml \
+--original-xml /vault/kvm-vm-templates/$TEMPLATE.xml \
 --name $HOSTNAME \
 --file /vault/kvm-vm-storage/$HOSTNAME.qcow2
 
@@ -22,3 +42,5 @@ virt-sysprep --domain $HOSTNAME \
 
 # Start node
 virsh start $HOSTNAME
+
+exit 0
